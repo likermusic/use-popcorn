1e47be620d930298c226991b51480f847e26123d
Sergey Timofeev <drumliker@yandex.ru>
2025-01-26 12:11:51 +0300

    add star rating markup

---
+++ src/components/StarRating/StarRating.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Star } from "./Star";
+
+export function StarRating() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        gap: "16px",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+        }}
+      >
+        {Array.from({ length: 10 }, (_, ind) => (
+          <Star key={ind} />
+        ))}
+      </div>
+      <span
+        style={{
+          verticalAlign: "5px",
+          color: "white",
+        }}
+      >
+        10/10
+      </span>
+    </div>
+  );
+}
